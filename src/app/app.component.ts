74f83e3f571863cef0a0cad1e503820cf8183859
JuanCamiloDevFrontBack <juank0215dev@gmail.com>
2023-09-26 16:19:52 -0500

    Se realiza un ejemplo de uso sobre render2 de angular para acceder al dom de forma correcta, como buena practicAA.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('btn2') btn2!: ElementRef;
   title = 'app-test-cod';
+
+  constructor(private readonly render: Renderer2) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    console.log('btn2 setT', this.btn2)
+    console.log('btn2 setT', this.btn2.nativeElement)
+    this.render.listen(this.btn2.nativeElement, 'click', () => this.eventClickFromController());
+    // console.log('btn2 setT', this.btn2)
+  }
+
+  eventClickFromTemplate(): void {
+    console.log('Hola me llaman desde el template :]');
+  }
+  
+  eventClickFromController(): void {
+    console.log('Hola me llaman desde el controller :]');
+  }
+
 }
