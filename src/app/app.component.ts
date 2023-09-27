cb42712df41792433c19f307e4f01531d11f2c12
JuanCamiloDevFrontBack <juank0215dev@gmail.com>
2023-09-26 21:07:11 -0500

    Se agrega otras forma d implementar el viewChild cn un método set.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,34 @@ import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild, inj
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent {
+// export class AppComponent implements AfterViewInit {
+
   @ViewChild('btn2') btn2!: ElementRef;
   title = 'app-test-cod';
+  @ViewChild('btn3') set setBtn2(btn: ElementRef) {
+    // TODO: con el método set no se requiera de implementar el ngAfterViewInit.
+    this.render.setStyle(btn.nativeElement, 'margin', '15px');
+    this.render.setStyle(btn.nativeElement, 'color', 'white');
+    this.render.setStyle(btn.nativeElement, 'background', 'green');
+    this.render.setStyle(btn.nativeElement, 'width', '120px');
+  }
 
   // constructor(private readonly render: Renderer2) {}
   private readonly render = inject(Renderer2);
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    console.log('btn2 setT', this.btn2)
-    console.log('btn2 setT', this.btn2.nativeElement)
-    this.render.listen(this.btn2.nativeElement, 'click', () => this.eventClickFromController());
-    // console.log('btn2 setT', this.btn2)
-  }
+  // ngAfterViewInit(): void {
+  //   this.render.listen(this.btn2.nativeElement, 'click', () => this.eventClickFromController());
+  // }
 
   eventClickFromTemplate(): void {
     console.log('Hola me llaman desde el template :]');
+    alert('Hola me llaman desde el template :]');
   }
   
   eventClickFromController(): void {
     console.log('Hola me llaman desde el controller :]');
+    alert('Hola me llaman desde el controller :]');
   }
 
 }
